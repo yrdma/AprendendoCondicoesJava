3887fe6b79ba57e1c35a9f6e6311194b2dfa9174
GabrielMombelli <gabriel.mombelli9@gmail.com>
2024-08-21 20:46:11 -0300

    Criação do projeto Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+    Integer idadePessoa = 20;
+    String NomePessoa = "Nivaldo";
+    /*
+    * > Maior
+    * >= Maior Igual
+    * < Menor
+    * <= Menor igual
+    * == é igual
+    * != é diferente
+    * equal Compara Textos
+     */
+    if (idadePessoa > 21){
+        System.out.println("É maior que 21");
+    }else {
+        System.out.println("Não é Maior");
+    }
+
+
+
     }
 }
